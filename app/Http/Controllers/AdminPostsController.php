33161c17fe31bdaa0e753e34ef6e18e33a4628da
Orhan <orhanbulbul@gmail.com>
2018-07-26 18:01:49 +0300

    able to create post with static category

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostCreateRequest;
+use App\Photo;
+use App\Post;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
 
 class AdminPostsController extends Controller
 {
@@ -15,7 +19,9 @@ class AdminPostsController extends Controller
      */
     public function index()
     {
-        return view('admin.posts.index');
+
+        $posts = Post::all();
+        return view('admin.posts.index',compact('posts'));
     }
 
     /**
@@ -34,9 +40,32 @@ class AdminPostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostCreateRequest $request)
     {
-        //
+        $input = $request->all(); //formda yollananları input değişkenine atıyoruz.
+
+        $user = Auth::user();
+
+        if($file = $request->file('photo_id')){
+
+           $name = time() . $file->getClientOriginalName();
+
+           $file->move('images', $name);
+
+           $photo = Photo::create(['file'=>$name]);
+
+           $input['photo_id'] = $photo->id; //input'daki photo_id bölümüne oluşturduğumuz
+                                            //resmi yolluyoruz. Bunu resimle postu
+                                            //ilişkilendirmek için yapıyoruz.
+        }
+
+        $user->posts()->create($input); //Burada posts() methodu kullanarak chain yani
+                                        //zincir yapıyoruz. Yani user üzerinden bir post
+                                        //oluşturuyoruz.
+
+        return redirect('/admin/posts');
+
+
     }
 
     /**
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,4 +4,41 @@
 
     <h1>Posts</h1>
 
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>PHOTO</th>
+            <th>USER</th>
+            <th>CATEGORY</th>
+            <th>TITLE</th>
+            <th>BODY</th>
+            <th>CREATED AT</th>
+            <th>UPDATED AT</th>
+        </tr>
+        </thead>
+        <tbody>
+        @if($posts)
+
+            @foreach($posts as $post)
+
+                <tr>
+                    <td>{{$post->id}}</td>
+                    <td><img height="100" width="100" src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400'}}" alt=""></td>
+                    <td>{{$post->user->name}}</td>
+                    <td>{{$post->category_id}}</td>
+                    <td>{{$post->title}}</td>
+                    <td>{{$post->body}}</td>
+                    <td>{{$post->created_at->diffForHumans()}}</td>
+                    <td>{{$post->updated_at->diffForHumans()}}</td>
+                </tr>
+
+            @endforeach
+
+
+        @endif
+
+        </tbody>
+    </table>
+
 @stop
