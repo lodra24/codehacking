258441149083d77f8456fd5455ceecf24873b8ae
Orhan <orhanbulbul@gmail.com>
2018-07-27 20:35:18 +0300

    Categories properly showing and create with smooth

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -2,48 +2,60 @@
 
 @section('content')
 
-    <h1>Create Post</h1>
-    <div class="row">
-        {!! Form::model($post,['method'=>'PATCH', 'action'=> ['AdminPostsController@update', $post->id], 'files'=>true]) !!}
+    <h1>Edit Post</h1>
 
-        <div class="form-group">
-            {!! Form::label('title', 'Title: ') !!}
-            {!! Form::text('title', null, ['class'=> 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('category', 'Category:') !!}
-            {!! Form::select('category_id', $categories, null, ['class'=>'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('photo_id', 'Photo:') !!}
-            {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('body', 'Description:') !!}
-            {!! Form::textarea('body', null, ['class'=>'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::submit('Update Post', ['class'=> 'btn btn-primary']) !!}
-        </div>
-
-        {!! Form::close() !!}
-    </div>
 
     <div class="row">
 
+        <div class="col-sm-3">
+            <img class="img-responsive" src="{{$post->photo->file}}" alt="">
+
+        </div>
+
+        <div class="col-sm-9">
+
+
+            {!! Form::model($post,['method'=>'PATCH', 'action'=> ['AdminPostsController@update', $post->id], 'files'=>true]) !!}
+
+            <div class="form-group">
+                {!! Form::label('title', 'Title: ') !!}
+                {!! Form::text('title', null, ['class'=> 'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('category', 'Category:') !!}
+                {!! Form::select('category_id', $categories, null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('photo_id', 'Photo:') !!}
+                {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('body', 'Description:') !!}
+                {!! Form::textarea('body', null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::submit('Update Post', ['class'=> 'btn btn-primary col-sm-2']) !!}
+            </div>
+
+            {!! Form::close() !!}
             {!! Form::open(['method'=>'DELETE', 'action'=> ['AdminPostsController@destroy', $post->id]]) !!}
 
 
-
-                <div class="form-group">
-                    {!! Form::submit('Delete Post', ['class'=> 'btn btn-danger']) !!}
-                </div>
-                {!! Form::close() !!}
+            <div class="form-group">
+                {!! Form::submit('Delete Post', ['class'=> 'btn btn-danger col-sm-2 margin-left']) !!}
+            </div>
+            {!! Form::close() !!}
+        </div>
     </div>
+
+
+
+
+
 
     <div class="row">
         @include('includes.form_errors')
