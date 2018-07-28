ab8bbb84a85dbf56ce1c14788ae515b107dc18fc
Orhan <orhanbulbul@gmail.com>
2018-07-28 13:33:30 +0300

    Categories working hundred percent

--- app/Http/Controllers/AdminCategoriesController.php
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -21,15 +21,7 @@ class AdminCategoriesController extends Controller
         return view('admin.categories.index', compact('categories'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
 
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -63,7 +55,9 @@ class AdminCategoriesController extends Controller
      */
     public function edit($id)
     {
-        //
+        $category = Category::findOrFail($id);
+
+        return view('admin.categories.edit', compact('category'));
     }
 
     /**
@@ -75,7 +69,9 @@ class AdminCategoriesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        Category::findOrFail($id)->update($request->all());
+
+        return redirect('/admin/categories');
     }
 
     /**
@@ -86,6 +82,8 @@ class AdminCategoriesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Category::findOrFail($id)->delete();
+
+        return redirect('/admin/categories');
     }
 }
--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -5,7 +5,7 @@
 
     <div class="col-sm-6">
 
-        {!! Form::model($category,['method'=>'PATCH', 'action'=> 'AdminCategoriesController@update', $category->id]) !!}
+        {!! Form::model($category,['method'=>'PATCH', 'action'=> ['AdminCategoriesController@update', $category->id]]) !!}
 
         <div class="form-group">
             {!! Form::label('name', 'Title: ') !!}
@@ -13,14 +13,20 @@
         </div>
 
         <div class="form-group">
-            {!! Form::submit('Create Category', ['class'=> 'btn btn-primary']) !!}
+            {!! Form::submit('Update Category', ['class'=> 'btn btn-primary']) !!}
         </div>
         {!! Form::close() !!}
     </div>
 
-    <div class="col-sm-6">
+    <div class="col-sm-6" style="margin-top:25px;">
+
+            {!! Form::open(['method'=>'DELETE', 'action'=> ['AdminCategoriesController@destroy', $category->id]]) !!}
 
 
+                <div class="form-group">
+                    {!! Form::submit('Delete Category', ['class'=> 'btn btn-danger']) !!}
+                </div>
+                {!! Form::close() !!}
     </div>
 
 
